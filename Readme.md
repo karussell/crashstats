287d94c48ee290de6b57cd41c18013825fc6a0af
Peter <graphhopper@gmx.de>
2019-02-01 17:59:39 +0100

    trying to add traffic count (too sporadic at the moment; need to do some floodfilling algo via Dijkstra)

--- Readme.md
+++ Readme.md
@@ -3,13 +3,29 @@
 Data from https://unfallatlas.statistikportal.de/ converted the SHP to GeoJSON with QGIS.
 (Don't like to add geotools as a dependency ;))
 
+## Results
+
+ * Using OSM data from early 2019 and crash data from 2017, the total length is 
+   approx. 13000km or if you count the motorways as "two oneways" like we do in the code it is approx. 26000km
+ * In Germany approx. 17000km of 26000km (total length) of the highway network are
+   without a speed limit, i.e. there are at least 65.4% of the motorways without a speed limit
+ * On road segments of a motorway without a limit there are 69.5% of the people die, 65.4% are badly
+   injured and 57% are lightly injure.
+
+## TODOs
+
+ * When calculating the percentage of dead or badly injured people we need
+   to consider [traffic count data](https://www.bast.de/BASt_2017/DE/Verkehrstechnik/Fachthemen/v2-verkehrszaehlung/Daten/2017_1/Jawe2017.html?cms_map=1&cms_filter=true&cms_jahr=Jawe2017&cms_land=&cms_strTyp=A&cms_str=&cms_dtvKfz=&cms_dtvSv=)
+ * use older openstreetmap data to better match the situation for the older crash statistics
+ * traffic sign data in openstreetmap might be wrong, check this by a view examples
+
 ## Installation
 
 The following steps can be omitted once 0.12 is out:
 
 ```
 git clone https://github.com/graphhopper/graphhopper/
-cd com.graphhopper
+cd graphhopper
 git checkout 843f994
 mvn clean install -DskipTests=true
 ```
--- src/main/java/com/graphhopper/crashstats/Start.java
+++ src/main/java/com/graphhopper/crashstats/Start.java
@@ -19,9 +19,7 @@ import org.locationtech.jts.geom.Coordinate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.zip.GZIPInputStream;
 
@@ -35,6 +33,7 @@ public class Start {
         final Map<String, IntEncodedValue> JSON_PROPERTIES = new HashMap<>();
 
         final String crashLocation = "Unfall-export.geojson.gz";
+        final String trafficCountsLocation = "Jawe2017.csv";
         final ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JtsModule());
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
@@ -47,13 +46,72 @@ public class Start {
                 // for data import
                 initLocationIndex();
 
+                DecimalEncodedValue trafficEnc = getEncodingManager().getEncodedValue("traffic", DecimalEncodedValue.class);
+                importTrafficCountFile("traffic", new File(trafficCountsLocation), trafficEnc);
+
                 IntEncodedValue crashEnc = getEncodingManager().getEncodedValue("crash", IntEncodedValue.class);
                 importFile("crashes", new File(crashLocation), crashEnc);
 
                 // skip for now super.postProcessing();
             }
 
-            public void importFile(String name, File file, IntEncodedValue enc) {
+            // TODO store traffic count on both sides of the motorway
+            void importTrafficCountFile(String name, File file, DecimalEncodedValue enc) {
+                StorableProperties props = getGraphHopperStorage().getProperties();
+                String storageKey = "files." + name + ".preprocessing";
+                String preprocessing = props.get(storageKey);
+                if (!preprocessing.isEmpty()) {
+                    logger.info("file " + file + " already preprocessed " + name);
+                    return;
+                }
+
+                String latStr = "Koor_WGS84_N", lonStr = "Koor_WGS84_E", trafficCountStr = "DTV_Kfz_MobisSo_Q";
+                int latIdx = -1, lonIdx = -1, invalid = 0, trafficCountIdx = -1;
+                LocationIndex index = getLocationIndex();
+
+                // try the following if we need the strings new InputStreamReader(new FileInputStream(file), Charset.forName("ISO-8859-1");)
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    String line;
+                    int lineCounter = 0;
+                    while ((line = reader.readLine()) != null) {
+                        String[] strs = line.split(";");
+                        if (lineCounter == 0) {
+                            latIdx = Arrays.asList(strs).indexOf(latStr);
+                            lonIdx = Arrays.asList(strs).indexOf(lonStr);
+                            trafficCountIdx = Arrays.asList(strs).indexOf(trafficCountStr);
+                            if (latIdx < 0 || lonIdx < 0 || trafficCountIdx < 0)
+                                throw new RuntimeException("Cannot find header " + latStr + " or " + lonStr + " or " + trafficCountStr);
+                            // skip header
+                            lineCounter++;
+                            continue;
+                        }
+                        lineCounter++;
+                        double lat = Double.parseDouble(strs[latIdx].replace(",", ".")),
+                                lon = Double.parseDouble(strs[lonIdx].replace(",", "."));
+                        QueryResult qr = index.findClosest(lat, lon, EdgeFilter.ALL_EDGES);
+                        // skip items too far away from the road or no traffic count in the line
+                        if (qr.getQueryDistance() > 20 || strs[trafficCountIdx].isEmpty()) {
+                            invalid++;
+                            continue;
+                        }
+
+                        EdgeIteratorState edge = qr.getClosestEdge();
+                        // remove separator for "thousands"
+                        try {
+                            edge.set(enc, (double) Integer.parseInt(strs[trafficCountIdx].replace(".", "")));
+                        } catch (NumberFormatException ex) {
+                            invalid++;
+                            logger.info(lineCounter + " problem with " + strs[trafficCountIdx]);
+                        }
+                    }
+                    props.put(storageKey, "done").flush();
+                    logger.info(name + " loaded " + (lineCounter - 1) + " (invalid " + invalid + ")");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            void importFile(String name, File file, IntEncodedValue enc) {
                 StorableProperties props = getGraphHopperStorage().getProperties();
                 String storageKey = "files." + name + ".preprocessing";
                 String preprocessing = props.get(storageKey);
@@ -103,6 +161,9 @@ public class Start {
         EncodingManager.Builder builder = new EncodingManager.Builder(8);
         IntEncodedValue crashEnc = new SimpleIntEncodedValue("crash", 6, false);
         builder.add(crashEnc);
+        // store with less precision (rounding via factor 40)
+        IntEncodedValue trafficCountEnc = new FactorizedDecimalEncodedValue("traffic", 12, 40, false);
+        builder.add(trafficCountEnc);
 
         // store JSON properties in graph
         // explained in German in meta.txt but what is UTYP vs. UTYP1?
@@ -112,7 +173,8 @@ public class Start {
             builder.add(enc);
         }
 
-        builder.add(new CarFlagEncoder());
+        CarFlagEncoder carFlagEncoder = new CarFlagEncoder();
+        builder.add(carFlagEncoder);
 
         // the next code will be likely already part of 0.12
         // START 0.12
@@ -154,19 +216,19 @@ public class Start {
 
         hopper.setCHEnabled(false);
         hopper.importOrLoad();
+        GraphHopperStorage graph = hopper.getGraphHopperStorage();
 
-//        new DepthFirstSearch() {
-//            @Override
-//            protected boolean checkAdjacent(EdgeIteratorState edge) {
-//                // fill max_speed gap, write previou value if same road type
-//                return true;
+        // spread traffic information until next; do this only for motorway
+//        RoadClass motorwayRC = roadClassMap.get("motorway");
+//        graph.createEdgeExplorer(edgeIteratorState -> edgeIteratorState.get(roadClassEnc).equals(motorwayRC));
+//        DijkstraBidirectionRef dijkstra = new DijkstraBidirectionRef(graph, new ShortestWeighting(carFlagEncoder), TraversalMode.NODE_BASED) {
+//            {   initFrom(nodeOfTrafficCounter, 0);
+//                runAlgo();
 //            }
-//        }.start(hopper.getGraphHopperStorage().createEdgeExplorer(),
-//                hopper.getLocationIndex().findClosest(52.515542, 13.388, EdgeFilter.ALL_EDGES).getClosestNode());
+//        };
 
         logger.info("start gathering statistics");
         Map<String, Integer> countMap = new HashMap<>();
-        GraphHopperStorage graph = hopper.getGraphHopperStorage();
         AllEdgesIterator iter = graph.getAllEdges();
         int motorwayUnlimited = 0;
         double meterUnlimited = 0;
@@ -180,6 +242,9 @@ public class Start {
             String key = iter.get(roadClassEnc).toString();
             Integer count = countMap.get(key);
             countMap.put(key, (count == null ? 0 : count) + iter.get(crashEnc));
+
+//            if (iter.get(trafficCountEnc) <= 0)
+//                continue;
 
             if (key.equals("motorway")) {
                 double val = iter.get(maxSpeedEnc);
